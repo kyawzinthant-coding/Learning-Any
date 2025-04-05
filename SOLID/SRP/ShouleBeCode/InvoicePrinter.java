de500ad3d3c7e2b54ebe559dc2aa4a557042a009
Kyaw Zin Thant <kyawthantver2@gmail.com>
2025-04-05 18:26:04 +0630

    nav-bar

--- SOLID/SRP/ShouleBeCode/InvoicePrinter.java
+++ SOLID/SRP/ShouleBeCode/InvoicePrinter.java
@@ -7,8 +7,5 @@ public class InvoicePrinter {
 
     public void print() {
         System.out.println(invoice.quantity + "x " + invoice.book.name + " " + invoice.book.price + " $");
-        System.out.println("Discount Rate: " + invoice.discountRate);
-        System.out.println("Tax Rate: " + invoice.taxRate);
-        System.out.println("Total: " + invoice.total + " $");
     }
 }
