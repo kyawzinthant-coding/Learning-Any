fb0b335f89efa997a56eac313888f999b8cb76db
Kyaw Zin Thant <kyawthantver2@gmail.com>
2025-04-08 23:26:28 +0630

    testimonials section done

--- practice-landing-page/src/sections/Testimonials.tsx
+++ practice-landing-page/src/sections/Testimonials.tsx
@@ -8,6 +8,7 @@ import avatar7 from "@/assets/avatar-7.png";
 import avatar8 from "@/assets/avatar-8.png";
 import avatar9 from "@/assets/avatar-9.png";
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
 const testimonials = [
   {
@@ -70,43 +71,63 @@ const firstColumn = testimonials.slice(0, 3);
 const secondColumn = testimonials.slice(3, 6);
 const thirdColumn = testimonials.slice(6, 9);
 
+const TestimonialsColun = (props: {
+  className?: string;
+  testinomials: typeof testimonials;
+}) => (
+  <div
+    className={twMerge(
+      "flex flex-col justify-center gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]",
+      props.className
+    )}
+  >
+    {props.testinomials.map(({ text, imageSrc, name, username }) => (
+      <div className="card" key={name}>
+        <div>{text}</div>
+        <div className="flex items-center gap-2 mt-5">
+          <Image
+            src={imageSrc}
+            alt={name}
+            width={40}
+            height={40}
+            className="h-10 w-10 rounded-full"
+          />
+          <div className="flex flex-col">
+            <div className="font-medium tracking-tight leading-5">{name}</div>
+            <div className="leading-5 tracking-tight">{username}</div>
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+);
+
 export const Testimonials = () => {
   return (
     <section className="bg-white">
       <div className="container">
-        <div className="flex justify-center">
-          <div className="tag">Testimonials</div>
+        <div className="section-heading">
+          <div className="flex justify-center">
+            <div className="tag">Testimonials</div>
+          </div>
+
+          <h2 className="section-title mt-5">What our users say</h2>
+          <p className="section-description mt-5">
+            From intuitive design to powerful features, our app has become an
+            essential tool for users around the world.
+          </p>
         </div>
 
-        <h2 className="section-title mt-5">What our users say</h2>
-        <p className="section-description mt-5">
-          From intuitive design to powerful features, our app has become an
-          essential tool for users around the world.
-        </p>
-
-        <div className="flex justify-center">
-          <div className="flex flex-col justify-center gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]">
-            {firstColumn.map(({ text, imageSrc, name, username }) => (
-              <div className="card" key={name}>
-                <div>{text}</div>
-                <div className="flex items-center gap-2 mt-5">
-                  <Image
-                    src={imageSrc}
-                    alt={name}
-                    width={40}
-                    height={40}
-                    className="h-10 w-10 rounded-full"
-                  />
-                  <div className="flex flex-col">
-                    <div className="font-medium tracking-tight leading-5">
-                      {name}
-                    </div>
-                    <div className="leading-5 tracking-tight">{username}</div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
+        <div className="flex justify-center gap-6">
+          <TestimonialsColun testinomials={firstColumn} />
+          <TestimonialsColun
+            testinomials={secondColumn}
+            className="hidden md:flex"
+          />
+          <TestimonialsColun
+            testinomials={thirdColumn}
+            className="hidden lg:flex"
+          />
         </div>
       </div>
     </section>
